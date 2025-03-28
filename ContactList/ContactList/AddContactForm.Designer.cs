db9b9373f5f2368614f32df4e91bf9193a08f7d9
Le Hoang <le294594@gmail.com>
2025-03-28 22:48:50 +0700

    Sửa Form thêm liên hệ, bổ sung trường Email

--- ContactList/ContactList/AddContactForm.Designer.cs
+++ ContactList/ContactList/AddContactForm.Designer.cs
@@ -31,11 +31,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(625, 328);
+            this.button1.Location = new System.Drawing.Point(379, 247);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,7 +45,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(360, 135);
+            this.textBox1.Location = new System.Drawing.Point(255, 112);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(219, 22);
             this.textBox1.TabIndex = 1;
@@ -52,17 +53,26 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(360, 209);
+            this.textBox2.Location = new System.Drawing.Point(255, 152);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(219, 22);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "SDT";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(255, 84);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.Text = "Email";
             // 
             // AddContactForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -79,5 +89,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtEmail;
     }
 }
